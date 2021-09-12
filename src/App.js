05652abf40c5a8c47617409de1ecd8cd6e7d0c4f
ShijuPJohn <shijupjohn4u@gmail.com>
2021-09-13 04:28:09 +0530

    initial commit: homepage, directory, items components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomepageComponent from "./pages/homepage/homepage.component";
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <HomepageComponent/>
+        </React.Fragment>
+    );
+}
+
+export default App;
---
+++ src/components/menu-item/menuitem.component.jsx
@@ -0,0 +1,14 @@
+import './menu-item.scss'
+
+export const MenuitemComponent = ({title, imageUrl, size}) => {
+    console.log(size)
+    return (
+        <div className={`${size} menu-item`}>
+            <div className="background-image"  style={{backgroundImage: `url(${imageUrl})`}}/>
+            <div className="content">
+                <h1 className="title">{title}</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+    )
+}
